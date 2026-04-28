548cc4eea7d3f56ba0748c6d3ca59c727cce1cbc
Manan <manannagpal003@gmail.com>
2026-04-29 00:14:52 +0530

    fix api url for production

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Changed to match backend port
+  baseURL: "import.meta.env.VITE_API_BASE_URL", // Changed to match backend port
   withCredentials: true,
 });
 
