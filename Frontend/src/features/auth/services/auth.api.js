21b0a06ed46fa204ac5a871615db5eefc160e215
Manan <manannagpal003@gmail.com>
2026-04-29 14:16:09 +0530

    fix cors for production

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "import.meta.env.VITE_API_BASE_URL", // Changed to match backend port
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Use env variable for backend URL
   withCredentials: true,
 });
 
